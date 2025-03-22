0619dfba69ca6e1784f31d088603fb58eb4384f8
izzzorry <isabel.zorrilla@uao.edu.co>
2025-03-22 09:20:54 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'proyeSA1',
+  webDir: 'www'
+};
+
+export default config;
